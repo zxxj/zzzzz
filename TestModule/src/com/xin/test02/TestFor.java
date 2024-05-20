4cba69f252e46d399b7f8066e848090edd5f078c
zhangxinxin <1285367184@qq.com>
2024-05-20 22:49:57 +0800

    feat: 循环

---
+++ TestModule/src/com/xin/test02/TestFor.java
@@ -0,0 +1,13 @@
+package com.xin.test02;
+
+public class TestFor {
+    public static void main(String[] args) {
+
+        int sum = 0;
+
+        for(int num = 1; num <= 5; num ++) {
+            sum += num;
+        }
+        System.out.println(sum);
+    }
+}
---
+++ TestModule/src/com/xin/test02/TestWhite.java
@@ -0,0 +1,16 @@
+package com.xin.test02;
+
+public class TestWhite {
+    public static void main(String[] args) {
+
+        int num = 1;
+        int sum = 0;
+
+        while (num <= 5) {
+            sum += num;
+            num ++;
+        }
+
+        System.out.println(sum);
+    }
+}
